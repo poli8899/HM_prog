95b2c3edbea86f049625a22aa0e24dbba7d78cb0
Polina <poli8899@yandex.ru>
2023-08-01 16:10:49 +0300

    7 урок

--- WebAdv/WebAdv/app_advertisements/models.py
+++ WebAdv/WebAdv/app_advertisements/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    title = models.CharField("заголовок", max_length=128)
+    description = models.TextField("описание")
+    price = models.DecimalField("цена", max_digits=10, decimal_places=2) # 999.999 max_digits=5, decimal_places=2 
+    auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Advertisemet(id={self.id}, title={self.title}, price={self.price})"
+
+    class Meta:
+        db_table = "advertisements"
